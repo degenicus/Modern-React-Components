d7f70ae03f649f9dffe0cf9e67e94f5e1c086e55
Magnus Brantheim <m.brantheim@gmail.com>
2019-07-01 21:38:15 +0200

    Refactored to comment component

--- src/index.js
+++ src/index.js
@@ -1,52 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker'
+import CommentDetail from './CommentDetail'
 
 const App = () => {
+    const cptHaddock = {
+        author: "Captain Haddock",
+        comment: "Billions of billious blue blistering barnacles in a thundering typhoon",
+        time: "Sometime",
+        image: "https://media.scotchwhisky.com/images/media/c1fd4c63ddf3fba2014c31fde56071e6.jpg"
+    }
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.cats()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Adolfen
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 13:37</span>
-                    </div>
-                    <div className="text">Mooost excellent!</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.abstract()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Saraswati
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Always but never</span>
-                    </div>
-                    <div className="text">The mind creates the appearance of duality...</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src="https://media.scotchwhisky.com/images/media/c1fd4c63ddf3fba2014c31fde56071e6.jpg"/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Captain Haddock
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Sometime</span>
-                    </div>
-                    <div className="text">Billions of billious blue blistering barnacles in a thundering typhoon</div>
-                </div>
-            </div>
+
+            <CommentDetail
+                author="Saraswati"
+                comment="The mind creates the appearance of duality..."
+                time="Always but never"
+                image={faker.image.abstract()}
+            />
+            <CommentDetail
+                author="Adolfen"
+                comment="Mooost excellent!"
+                time="Today at 13:37"
+                image={faker.image.cats()}
+            />
+            <CommentDetail
+                author={cptHaddock.author}
+                comment={cptHaddock.comment}
+                time={cptHaddock.time}
+                image={cptHaddock.image}
+            />
         </div>
     )
 }

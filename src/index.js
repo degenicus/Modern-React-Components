a5b2dafdcbafec1a8c81d607e5ba3c112fc6f906
Magnus Brantheim <m.brantheim@gmail.com>
2019-07-01 20:25:03 +0200

    Added semantic ui, made jsx for comments.

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Adolfen
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 13:37</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"))
